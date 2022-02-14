0ef91550dceecd962a12eb8a50f99646806880d9
NguyenPhuongNam99 <nguyenphuongnam06012000@gmail.com>
2022-02-15 00:32:36 +0700

    deploy heroku and implement function

--- app.js
+++ app.js
@@ -2,6 +2,16 @@ const express = require('express');
 const app = express()
 const connectDB = require('./config/db')
 const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
+
+//khoi dong methodOverrride
+app.use(methodOverride('_method'))
+
+
+//khoi dong body parse
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
 
 //khoi dong handlebars
 
@@ -24,6 +34,6 @@ app.use('/posts', post)
 app.get('/', (req, res) => res.render('index'))
 app.get('/about', (req,res) => res.render('about'))
 
-const PORT = 4000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`server runing ${PORT}`))
--- routes/post.js
+++ routes/post.js
@@ -7,4 +7,43 @@ router.get('/add', (req, res) => {
     res.render('posts/add')
 })
 
+//hien thi tat ca cac bai viet
+router.get('/', async (req, res) => {
+    const posts = await Post.find().lean().sort({date: -1})
+    res.render('posts/index', {posts})
+})
+
+//tao post moi
+router.post('/', async(req, res) => {
+    const {title, text}= req.body;
+    let errors = []
+    if(!title) errors.push({msg: 'title require'})
+    if(!text) errors.push({msg: 'text require'})
+    if(errors.length > 0) res.render('posts/add',{title,text})
+    else {
+        const newPostData = {title, text}
+        const newPost = new Post(newPostData)
+        await newPost.save()
+        res.redirect('/posts')
+    }
+})
+
+
+router.get('/edit/:id' , async ( req, res) => {
+    const post = await Post.findOne({_id: req.params.id}).lean()
+    res.render('posts/edit', {post})
+})
+
+router.put('/:id', async(req, res) => {
+    const {title, text} = req.body
+    await Post.findOneAndUpdate({_id: req.params.id},{title, text})
+    res.redirect('/posts')
+})
+
+
+router.delete('/:id', async(req, res) => {
+    await Post.findOneAndRemove({_id: req.params.id})
+    res.redirect('/posts')
+})
+
 module.exports = router
